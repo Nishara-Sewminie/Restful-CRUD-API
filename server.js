1feb7be778556513248f7524dad928ae83034df6
MadhushanES <madhushan.es.pilapitiya@gmail.com>
2023-11-24 15:21:23 +0530

    Madhushan

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const {
+  CustomerModel,
+  AdminModel,
+  TransactionModel,
+  BillTypeModel,
+  ServiceProviderModel,
+} = require("./models/productModel");
+
 const app = express();
 
 app.use(express.json());
@@ -5,4 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello NODE API");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog, My name is Janith");
+});
+
+app.get("/customers", async (req, res) => {
+  try {
+    const customers = await CustomerModel.find({});
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
