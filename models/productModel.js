e0bbc8bb3a51a66502725844f95369327125f479
janithkanchana <127092899+janithkanchana@users.noreply.github.com>
2023-11-24 16:55:32 +0530

    Janith

--- models/productModel.js
+++ models/productModel.js
@@ -1,1 +1,14 @@
-
+const mongoose = require("mongoose");
+const customerSchema = new mongoose.Schema({
+//   customerID: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     unique: true,
+//   },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  userType: { type: String, required: true },
+  currentAccountStatus: { type: String, required: true },
+  approvalStatus: { type: String, required: true },
+});
--- server.js
+++ server.js
@@ -40,3 +40,20 @@ app.get("/customers/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+app.post("/customers", async (req, res) => {
+  try {
+    const customer = await CustomerModel(req.body);
+    res.status(200).json(customer);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+app.get("/cc", (req, res) => {
+  res.send("Hello");
+});
+
+
+
+
